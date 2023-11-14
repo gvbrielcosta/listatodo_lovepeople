2b91ac7512fca685bef20192fda6e77a6806af40
Jonatas <joba.herc@gmail.com>
2023-11-14 17:42:18 -0300

    novas implementacoes

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -11,38 +11,61 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 232, 184, 240),
-        body: SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.white, Color.fromARGB(255, 232, 184, 240)],
+      backgroundColor: Color.fromRGBO(178, 73, 227, 1),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                Container(
+                  height: 300,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(400),
+                          bottomRight: Radius.circular(400),
+                          topLeft: Radius.zero,
+                          topRight: Radius.zero)),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
+                Positioned(
+                  top: 100,
+                  child: Image.network(
+                    'https://cdn-fhadf.nitrocdn.com/ZposurrhgpMNIxJkJSDhfrsVaOicbFuO/assets/images/optimized/rev-bb4d11b/www.lovepeople.com.br/wp-content/uploads/2021/02/favicon-lovepeople.png',
+                    height: 150,
+                  ),
+                ),
+                Positioned(
+                  top: 300,
+                  child: Text(
+                    'Que bom que voltou!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize:
+                          34.0, // Ajuste o tamanho da fonte conforme necessário
+                      fontWeight: FontWeight.bold, // Adiciona negrito ao texto
+                    ),
+                  ),
+                ),
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: Icon(
-                        Icons.login,
-                        size: 40.0,
-                      ),
-                    ),
                     SizedBox(
-                      height: 30,
+                      height: 200,
                     ),
                     TextField(
                       keyboardType: TextInputType.emailAddress,
                       decoration: InputDecoration(
-                          labelText: 'Email', border: OutlineInputBorder()),
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: 'Número de telefone, email ou CPF',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
                     ),
                     SizedBox(
                       height: 30,
@@ -50,17 +73,28 @@ class _LoginPageState extends State<LoginPage> {
                     TextField(
                       obscureText: true,
                       decoration: InputDecoration(
-                          labelText: 'Password', border: OutlineInputBorder()),
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
                     ),
                     SizedBox(
                       height: 30,
                     ),
-                    OutlinedButton(onPressed: () {}, child: Text('Login'))
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                    ),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/pages/signup/signup_page.dart
+++ lib/pages/signup/signup_page.dart
@@ -8,14 +8,20 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(169, 1, 247, 1)),
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromRGBO(169, 1, 247, 1)),
       home: Scaffold(
         body: Center(
           child: Container(
             height: 400,
             alignment: Alignment.center,
             child: Column(children: [
-              Text('Vamos começar!', style: GoogleFonts.montserrat(fontSize: 30, color: Colors.white, fontWeight: FontWeight.w600, letterSpacing: 1.0)),
+              Text('Vamos começar!',
+                  style: GoogleFonts.montserrat(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0)),
               const SizedBox(
                 height: 20,
               ),
@@ -23,7 +29,19 @@ class SignUpPage extends StatelessWidget {
                 height: 40,
                 width: 350,
                 child: TextFormField(
-                  decoration: const InputDecoration(contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0), hintText: 'Nome', hintStyle: TextStyle(fontFamily: 'Tahoma', fontSize: 18, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w400), filled: true, fillColor: Colors.white, border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(10)))),
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0),
+                      hintText: 'Nome',
+                      hintStyle: TextStyle(
+                          fontFamily: 'Tahoma',
+                          fontSize: 18,
+                          color: Color.fromRGBO(49, 1, 185, 1),
+                          fontWeight: FontWeight.w400),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(10)))),
                 ),
               ),
               const SizedBox(
@@ -33,7 +51,19 @@ class SignUpPage extends StatelessWidget {
                 height: 40,
                 width: 350,
                 child: TextFormField(
-                  decoration: const InputDecoration(contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0), hintText: 'Número de telefone, e-mail ou CPF', hintStyle: TextStyle(fontFamily: 'Tahoma', fontSize: 18, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w400), filled: true, fillColor: Colors.white, border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(10)))),
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0),
+                      hintText: 'Número de telefone, e-mail ou CPF',
+                      hintStyle: TextStyle(
+                          fontFamily: 'Tahoma',
+                          fontSize: 18,
+                          color: Color.fromRGBO(49, 1, 185, 1),
+                          fontWeight: FontWeight.w400),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(10)))),
                 ),
               ),
               const SizedBox(
@@ -46,14 +76,20 @@ class SignUpPage extends StatelessWidget {
                   decoration: const InputDecoration(
                       contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0),
                       hintText: 'Senha',
-                      hintStyle: TextStyle(fontFamily: 'Tahoma', fontSize: 18, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w400),
+                      hintStyle: TextStyle(
+                          fontFamily: 'Tahoma',
+                          fontSize: 18,
+                          color: Color.fromRGBO(49, 1, 185, 1),
+                          fontWeight: FontWeight.w400),
                       filled: true,
                       fillColor: Colors.white,
                       suffixIcon: Icon(
                         Icons.remove_red_eye,
                         color: Color.fromRGBO(49, 1, 185, 1),
                       ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(10)))),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(10)))),
                 ),
               ),
               const SizedBox(
@@ -66,14 +102,20 @@ class SignUpPage extends StatelessWidget {
                   decoration: const InputDecoration(
                       contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0),
                       hintText: 'Confirmar senha',
-                      hintStyle: TextStyle(fontFamily: 'Tahoma', fontSize: 18, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w400),
+                      hintStyle: TextStyle(
+                          fontFamily: 'Tahoma',
+                          fontSize: 18,
+                          color: Color.fromRGBO(49, 1, 185, 1),
+                          fontWeight: FontWeight.w400),
                       filled: true,
                       fillColor: Colors.white,
                       suffixIcon: Icon(
                         Icons.remove_red_eye,
                         color: Color.fromRGBO(49, 1, 185, 1),
                       ),
-                      border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(10)))),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(10)))),
                 ),
               ),
               const SizedBox(
@@ -83,7 +125,8 @@ class SignUpPage extends StatelessWidget {
                 width: 150,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
                       backgroundColor: const Color.fromRGBO(49, 1, 185, 1),
                       side: const BorderSide(
                         color: Colors.white,

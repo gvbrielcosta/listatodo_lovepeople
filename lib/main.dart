bd8e6080d0ce4b2254b31684565fb88d88b46064
Jonatas <joba.herc@gmail.com>
2023-11-17 23:08:04 -0300

    implemetacao

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
-      home: TodoPage(),
     );
   }
 }

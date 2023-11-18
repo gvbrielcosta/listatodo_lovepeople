3b831c543e6e220ca2597c40856ed565adb58b04
Jonatas <joba.herc@gmail.com>
2023-11-17 23:54:01 -0300

    alteracoes

--- lib/pages/todo/todo_page.dart
+++ lib/pages/todo/todo_page.dart
@@ -3,9 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:listatodo_lovepeople/data/datasource/session_datasource.dart';
 import 'package:listatodo_lovepeople/data/model/todo_response.dart';
 import 'package:listatodo_lovepeople/data/repositories/todo_repository.dart';
+import 'package:listatodo_lovepeople/pages/removTarefa/removTarefa.dart';
 
 class TodoPage extends StatefulWidget {
-  const TodoPage({super.key});
+  const TodoPage({Key? key}) : super(key: key);
 
   @override
   State<TodoPage> createState() => _TodoPageState();
@@ -28,7 +29,9 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(169, 1, 247, 1)),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(169, 1, 247, 1),
+      ),
       home: Scaffold(
         body: SingleChildScrollView(
           child: Column(
@@ -39,17 +42,26 @@ class _TodoPageState extends State<TodoPage> {
                   Padding(
                     padding: EdgeInsets.fromLTRB(350, 40, 0, 0),
                     child: IconButton(
-                        onPressed: _logout,
-                        icon: const Icon(
-                          Icons.exit_to_app,
-                          size: 40,
-                          color: Colors.white,
-                        )),
+                      onPressed: _logout,
+                      icon: const Icon(
+                        Icons.exit_to_app,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 90,
                     width: 90,
-                    decoration: const BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.zero, topRight: Radius.zero, bottomLeft: Radius.zero, bottomRight: Radius.circular(200)), color: Colors.white),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.zero,
+                        topRight: Radius.zero,
+                        bottomLeft: Radius.zero,
+                        bottomRight: Radius.circular(200),
+                      ),
+                      color: Colors.white,
+                    ),
                   ),
                   const Padding(
                     padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
@@ -64,7 +76,12 @@ class _TodoPageState extends State<TodoPage> {
                       padding: const EdgeInsets.only(top: 50),
                       child: Text(
                         'Suas listagens',
-                        style: GoogleFonts.montserrat(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600, letterSpacing: 1.0),
+                        style: GoogleFonts.montserrat(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.0,
+                        ),
                       ),
                     ),
                   ),
@@ -80,10 +97,20 @@ class _TodoPageState extends State<TodoPage> {
                         decoration: const InputDecoration(
                           contentPadding: EdgeInsets.fromLTRB(12, 15, 0, 0),
                           hintText: 'Busque palavras-chaves',
-                          hintStyle: TextStyle(fontFamily: 'Tahoma', fontSize: 18, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w400),
+                          hintStyle: TextStyle(
+                            fontFamily: 'Tahoma',
+                            fontSize: 18,
+                            color: Color.fromRGBO(49, 1, 185, 1),
+                            fontWeight: FontWeight.w400,
+                          ),
                           filled: true,
                           fillColor: Colors.white,
-                          border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.all(Radius.circular(16))),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(16),
+                            ),
+                          ),
                           suffixIcon: Icon(
                             Icons.search,
                             color: Color.fromRGBO(49, 1, 185, 1),
@@ -92,27 +119,55 @@ class _TodoPageState extends State<TodoPage> {
                       ),
                     ),
                     Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: todoList.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(22))),
-                              child: ListTile(
-                                contentPadding: EdgeInsets.all(17),
-                                tileColor: const Color.fromRGBO(255, 242, 204, 100),
-                                trailing: const Icon(
-                                  Icons.delete_outline,
-                                  color: Color.fromRGBO(49, 1, 185, 1),
-                                ),
-                                title: Text(todoList[index].attributes?.title ?? '', style: GoogleFonts.montserrat(fontSize: 17, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w700, letterSpacing: 1.0)),
-                                subtitle: Text(todoList[index].attributes?.description ?? '', style: GoogleFonts.montserrat(fontSize: 12, color: Color.fromRGBO(49, 1, 185, 1), fontWeight: FontWeight.w500, letterSpacing: 1.0)),
-                                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(22))),
+                      padding: const EdgeInsets.only(top: 20),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: todoList.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(22),
                               ),
-                            );
-                          },
-                        )),
+                            ),
+                            child: ListTile(
+                              contentPadding: EdgeInsets.all(17),
+                              tileColor:
+                                  const Color.fromRGBO(255, 242, 204, 100),
+                              trailing: RemovTarefa(
+                                onRemover: () =>
+                                    _removerTarefa(todoList[index]),
+                                confirmarRemover: 'Confirma a remoção?',
+                                todo: todoList[index],
+                              ),
+                              title: Text(
+                                todoList[index].attributes?.title ?? '',
+                                style: GoogleFonts.montserrat(
+                                  fontSize: 17,
+                                  color: Color.fromRGBO(49, 1, 185, 1),
+                                  fontWeight: FontWeight.w700,
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                todoList[index].attributes?.description ?? '',
+                                style: GoogleFonts.montserrat(
+                                  fontSize: 12,
+                                  color: Color.fromRGBO(49, 1, 185, 1),
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(22),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
                     const Padding(
                       padding: EdgeInsets.only(top: 30),
                       child: Icon(
@@ -139,7 +194,13 @@ class _TodoPageState extends State<TodoPage> {
     });
   }
 
-  _logout() async{
+  void _removerTarefa(Todo todo) {
+    setState(() {
+      todoList.remove(todo);
+    });
+  }
+
+  void _logout() async {
     await _sessionDataSource.deleteToken();
     if (context.mounted) {
       Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
